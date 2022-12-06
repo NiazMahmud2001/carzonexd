f4e17403646b6caf5ce05d4c669d74e55972550d
mohammed <u19200083@sharjah.ac.ae>
2022-12-06 10:29:56 +0400

    an addition with js

---
+++ U19200083/pages/java.js
@@ -0,0 +1,24 @@
+
+    var car = document.getElementById("card");
+
+    function openRegister() // aunction to rotate the card 
+    {
+      card.style.transform = "rotateY(-180deg)";
+    }
+    function openLogin() // a function to rerotate the card again 
+    {
+      card.style.transform = "rotateY(0deg)";
+    }
+    
+
+function register()  // a function to welcome the user once he register in our websites 
+{
+    document.getElementById("s");
+    alert("thanks for being one of our clients click ok to continue ");
+}
+
+function welcoming() // a function to welcome the 
+{
+    document.getElementById('d');
+    alert("Welcome back we are so glad to visit us again");
+}
